c4c091eab18f6d54b18cf37904cbff5a4c2c9893
RakibRahman <md.rr.talukder@gmail.com>
2021-09-05 14:04:30 +0600

    products data refactored

---
+++ src/helpers/productsData.js
@@ -0,0 +1,75 @@
+import P1 from "./../assets/products/A-160.webp";
+import P2 from "./../assets/products/B-177.webp";
+import P3 from "./../assets/products/SS-287.webp";
+import P4 from "./../assets/products/B-175.webp";
+import P5 from "./../assets/products/BC-123.webp";
+import P6 from "./../assets/products/D-551.webp";
+import P7 from "./../assets/products/SB-110.webp";
+import P8 from "./../assets/products/SS-295.webp";
+import P9 from "./../assets/products/BFL-750.webp";
+
+export const productsData = [
+  {
+    model: "SKU: A-160",
+    price: "25300",
+    pic: P1,
+  },
+  {
+    model: "SKU: B-175",
+    price: "5300",
+    pic: P2,
+  },
+  {
+    model: "SKU: SS-287",
+    price: "39000",
+    pic: P3,
+  },
+  {
+    model: "SKU: SB-110",
+    price: "9000",
+    pic: P4,
+  },
+  {
+    model: "SKU: B-177",
+    price: "25300",
+    pic: P5,
+  },
+  {
+    model: "SKU: BFL-750",
+    price: "42300",
+    pic: P6,
+  },
+  {
+    model: "SKU: SS-295",
+    price: "21300",
+    pic: P7,
+  },
+  {
+    model: "SKU: BC-123",
+    price: "16900",
+    pic: P8,
+  },
+  {
+    model: "SKU: D-551",
+    price: "9300",
+    pic: P9,
+  },
+];
+
+export const bestSelling = [
+  {
+    pic: P1,
+    model: "SKU: A-160",
+    price: "22,500",
+  },
+  {
+    pic: P2,
+    model: "SKU: B-177",
+    price: "12,900",
+  },
+  {
+    pic: P3,
+    model: "SKU: SS-287",
+    price: "2,500",
+  },
+];

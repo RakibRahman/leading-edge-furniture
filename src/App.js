19d12ee41a5431c5dbcfee2034c300a89b245f78
RakibRahman <md.rr.talukder@gmail.com>
2021-09-06 14:09:41 +0600

    dark mode toggle added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
@@ -8,10 +8,21 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  useEffect(() => {
+    if (darkMode) {
+      document.querySelector("#root").classList.add("dark");
+    } else {
+      document.querySelector("#root").classList.remove("dark");
+    }
+  }, [darkMode]);
+  const toggleHandler = () => {
+    setDarkMode(!darkMode);
+  };
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <Navbar toggleDarkMode={darkMode} clickHandler={toggleHandler} />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/products" component={Products} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
 import LogoPic from "./../assets/logo.png";
+import LogoPic2 from "./../assets/logo2.png";
 import { Link } from "react-router-dom";
 import MenuOpenIcon from "@material-ui/icons/MenuOpen";
 import CloseIcon from "@material-ui/icons/Close";
-function Navbar() {
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Brightness5Icon from "@material-ui/icons/Brightness5";
+import { Tooltip } from "@material-ui/core";
+
+function Navbar({ toggleDarkMode, clickHandler }) {
   const [toggle, setToggle] = useState(true);
   const [showLinks, setShowLinks] = useState(false);
 
   const Logo = () => (
     <div className="w-md md:w-1/4 ">
       <Link to="/">
-        {" "}
-        <img className="object-cover w-full h-auto" src={LogoPic} alt="ss" />
+        <img
+          className="object-cover w-full h-auto"
+          src={toggleDarkMode ? LogoPic2 : LogoPic}
+          alt="ss"
+        />
       </Link>
     </div>
   );
   const Links = ({ id }) => (
     <div
       id={id}
-      className="flex items-center justify-between gap-2 text-sm text-white transition md:gap-4 md:text-xl font-ubuntu"
+      className="flex items-center justify-between gap-2 text-sm text-white transition md:gap-4 md:text-xl font-ubuntu dark:text-black"
     >
       <Link className="text-red-500 hover:text-yellow-300" to="/">
         Home
@@ -32,7 +40,11 @@ function Navbar() {
       <Link className="hover:text-yellow-300 " to="/contact">
         Contact
       </Link>
-      <button>mode</button>
+      <Tooltip title="Toggle Light/Dark Mode">
+        <button onClick={clickHandler}>
+          {toggleDarkMode ? <Brightness5Icon /> : <Brightness4Icon />}
+        </button>
+      </Tooltip>
     </div>
   );
   const handleClickToggle = () => {
@@ -43,9 +55,13 @@ function Navbar() {
   return (
     <nav className="navbar">
       {toggle ? <Logo /> : null}
-      {showLinks ? <Links id="mobile__links" /> : null};
+      {showLinks ? <Links id="mobile__links" /> : null}
       <Links id="pc__links" />
-      <button onClick={handleClickToggle} id="menu__btn" className="text-white">
+      <button
+        onClick={handleClickToggle}
+        id="menu__btn"
+        className="text-white "
+      >
         {toggle ? <MenuOpenIcon /> : <CloseIcon />}
       </button>
     </nav>
